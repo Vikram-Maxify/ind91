72a6824dedb757f5b12a9d6ef859e4da4a63a8cb
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-05-20 13:01:59 +0530

    updated code

--- controllers/userController.js
+++ controllers/userController.js
@@ -6889,7 +6889,7 @@ const zilpay = async (req, res) => {
     }
 
     const params = {
-      amount: Number(money),
+      amount: Number(12),
       auth: "7RHBWPT9Q7LUNBZQFFVC",
       callback: "https://ind91.us/api/webapi/zilpayCallback",
       redirect_url: "https://ind91.us",
